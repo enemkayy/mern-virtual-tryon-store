42b52c191b26eaacca0869cc73cfd979f8e3e5b6
Nguyen Minh Khoi <12a5nguyenminhkhoi12@gmail.com>
2026-05-03 23:50:41 +0700

    feat: add home page components including Hero, BestSeller, and LatestCollection; implement Navbar and ShopContext

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { ShopContext } from "./Context";
+import { products } from "../assets/frontend_assets/assets";
+
+
+
+
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
+
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+  };
+
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
