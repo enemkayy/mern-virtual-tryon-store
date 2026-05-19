463cbd4a05f22607c2440bb82882e6018a962b17
Nguyen Minh Khoi <12a5nguyenminhkhoi12@gmail.com>
2026-05-20 01:45:18 +0700

    Add backend auth routes and project ignore rules

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,6 +10,8 @@ const userSchema = new mongoose.Schema(
   { minimize: false },
 );
 
+// This line checks if a model named "user" already exists in mongoose's models. If it does, it uses that existing model.
+// If it doesn't, it creates a new model using the userSchema and assigns it to the name "user".
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default userModel;
