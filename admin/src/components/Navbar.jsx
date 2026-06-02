f6da8db88a3eee67c41260921f10a2a9590f24be
VSieu <vqs1208@gmail.com>
2026-06-02 13:18:44 +0700

    Update admin product  management

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-
 import { assets } from "../assets/admin_assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img className="w-[max(10%,80px)]" src={assets.logo} alt="Logo" />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+      <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
+      <button onClick={() => setToken('')} className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm cursor-pointer hover:bg-gray-800 hover:scale-105 hover:shadow-md transition-all duration-300">
         Logout
       </button>
     </div>
